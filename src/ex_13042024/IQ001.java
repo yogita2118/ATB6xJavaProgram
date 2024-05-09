152d794c1f9ec9915534166d3ccf1ed29f15bf6c
Yogita Solanki <yogita2118@gmail.com>
2024-05-09 14:19:36 -0400

    09 May 2024

---
+++ src/ex_13042024/IQ001.java
@@ -0,0 +1,25 @@
+package ex_13042024;
+
+public class IQ001 {
+    public static void main(String[] args){
+        // Store the Age ?
+        byte age1 = 65 ;
+        //short age2 = 65 ;
+        //int age3 = 65 ;
+        //long age4 = 65 ;
+
+        // Phone Number -
+        long phone_number = 9812345678L;
+        long phone_number2 = 9867345789L;
+
+        float pi = 3.14f;
+        float pi_ = 3.14F;
+
+        double d = 1.234567876;
+
+
+
+
+
+    }
+}
---
+++ src/ex_13042024/Lab023.java
@@ -0,0 +1,20 @@
+package ex_13042024;
+
+public class Lab023 {
+    public static void main(String[] args) {
+        double a = 34 ;
+        double b = 10 ;
+        double result = a%b ;
+        System.out.println(result);
+
+        int num = 38 ;
+        int r = num%2;
+        System.out.println(r);
+
+        // Comparator Op.
+
+        // num%2 == 0 --> Even
+        // num%2 == 1 --> Odd
+
+    }
+}
