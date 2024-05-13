242c0e990c860dc60fe38af10851be4309d1858d
Yogita Solanki <yogita2118@gmail.com>
2024-05-13 14:05:06 -0400

    13 May 2024

---
+++ src/ex_18042024/Lab042.java
@@ -0,0 +1,15 @@
+package ex_18042024;
+
+public class Lab042 {
+    public static void main(String[] args) {
+
+        int a1 = 300;
+        byte b1 = (byte)a1;  // Explicit Casting
+        System.out.println(b1);
+
+        // 300 --> 32 Bits
+        // byte - 8 bits
+        // // 0 0 1 0 1 1 0 0
+        // 44
+    }
+}
