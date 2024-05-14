7522e797923f9c73667ae44c5861cfad4efd43d7
Yogita Solanki <yogita2118@gmail.com>
2024-05-14 10:40:47 -0400

    14 May 2024

---
+++ src/ex_20042024/Lab054.java
@@ -0,0 +1,14 @@
+package ex_20042024;
+
+public class Lab054 {
+    public static void main(String[] args) {
+        String name = "Yogi";
+        // How they are different?
+        // String - immutable in nature?
+        // Strings are immutable , non-primitive data types in Java.
+        // Immutable - that can't be change.
+        name = "Solanki";
+
+
+    }
+}
