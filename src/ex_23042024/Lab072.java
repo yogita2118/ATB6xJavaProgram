2978af679ce56e34210ff8ebe3055569b23cc3a6
Yogita Solanki <yogita2118@gmail.com>
2024-05-14 12:21:55 -0400

    14 May 2024

---
+++ src/ex_23042024/Lab072.java
@@ -0,0 +1,18 @@
+package ex_23042024;
+
+public class Lab072 {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        a = !a;
+
+        if (2 + 2 >= 4) { // 4 >4 or 4 = 4 --> true - OR GATE
+            System.out.println("Inside the loop code will be execute when the condition is true");
+
+
+        }
+
+        System.out.println("This is outside the loop, it will be executed always \t" +a);
+    }
+
+}
