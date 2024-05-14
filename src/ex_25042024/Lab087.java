5eb739715cacadec2b7c366f7f60869885de836c
Yogita Solanki <yogita2118@gmail.com>
2024-05-14 16:20:02 -0400

    14 May 2024

---
+++ src/ex_25042024/Lab087.java
@@ -0,0 +1,22 @@
+package ex_25042024;
+
+public class Lab087 {
+    public static void main(String[] args) {
+        int itemCode = 001;
+        // JDK > 13
+
+        switch (itemCode){
+            case 001 -> System.out.println("It's a laptop!");
+            case 002 ->System.out.println("It's a Desktop");
+            case 003,004 -> System.out.println("It's a Phone");
+            default -> System.out.println("Hello");
+        }
+
+        char ch = 'a';
+        switch (ch) {
+            case 'a','e','i','o','u' -> System.out.println("Vowel");
+            default -> System.out.println("Consonant");
+
+        }
+    }
+}
